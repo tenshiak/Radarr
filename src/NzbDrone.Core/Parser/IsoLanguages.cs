5457c950b22dd32c63a7ed3e0886f2a2fa6d7fa4
Qstick <qstick@gmail.com>
2020-11-18 00:35:18 -0500

    New: Portuguese (Brazil) Language Support

--- src/NzbDrone.Core/Parser/IsoLanguages.cs
+++ src/NzbDrone.Core/Parser/IsoLanguages.cs
@@ -11,7 +11,7 @@ namespace NzbDrone.Core.Parser
                                                                new IsoLanguage("en", "", "eng", "English", Language.English),
                                                                new IsoLanguage("fr", "fr", "fra", "French", Language.French),
                                                                new IsoLanguage("es", "", "spa", "Spanish", Language.Spanish),
-                                                               new IsoLanguage("de", "", "deu", "German", Language.German),
+                                                               new IsoLanguage("de", "de", "deu", "German", Language.German),
                                                                new IsoLanguage("it", "", "ita", "Italian", Language.Italian),
                                                                new IsoLanguage("da", "", "dan", "Danish", Language.Danish),
                                                                new IsoLanguage("nl", "", "nld", "Dutch", Language.Dutch),
@@ -35,7 +35,8 @@ namespace NzbDrone.Core.Parser
                                                                new IsoLanguage("hi", "", "hin", "Hindi", Language.Hindi),
                                                                new IsoLanguage("th", "", "tha", "Thai", Language.Thai),
                                                                new IsoLanguage("bg", "", "bul", "Bulgarian", Language.Bulgarian),
-                                                               new IsoLanguage("ro", "", "ron", "Romanian", Language.Romanian)
+                                                               new IsoLanguage("ro", "", "ron", "Romanian", Language.Romanian),
+                                                               new IsoLanguage("pt", "br", "", "Portuguese (Brazil)", Language.PortugueseBR)
                                                            };
 
         public static IsoLanguage Find(string isoCode)
